4c02108215e43bf79a92f9a4edcb9b444e496601
DanielaMendozaA <danni.ddma.96@gmail.com>
2024-05-17 15:19:39 -0500

    add new scenes

---
+++ Front/app/scenes/private/home2/home2.js
@@ -0,0 +1,23 @@
+import styles from './home2.css';
+
+export function Home2Scene(){
+
+    const pageContent = `
+    
+    <div id class=${styles.container}>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt culpa natus quo, libero nisi cumque, modi similique earum excepturi at architecto officia! Labore repudiandae, ab dicta possimus porro illum tempora.
+        Aliquid iure sint maxime, soluta, dolore quaerat facilis voluptate atque maiores nihil repellat quisquam, nesciunt sapiente fugit totam illo minus tempore! Officiis doloribus esse similique facere. Sunt vitae quos eos!
+        Vero neque animi nam expedita? Architecto tenetur voluptatibus vitae, animi reprehenderit, ex expedita similique tempora deleniti labore obcaecati. Laborum qui aliquam ducimus fuga sequi neque? Obcaecati ipsam esse nulla vitae.
+        Recusandae minus modi molestias? Nobis exercitationem ducimus, ipsam, molestias ipsum, et beatae cumque quis nostrum quibusdam illum repudiandae maxime aperiam culpa necessitatibus? Iure, delectus. Eos velit repudiandae maiores saepe inventore?
+        </p>
+    </div>
+    
+    `
+   
+}
+
+
+
+
+
